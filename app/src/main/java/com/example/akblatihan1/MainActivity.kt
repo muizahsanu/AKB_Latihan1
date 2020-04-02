2f3c3b016265551ce814775117a3f4c629e724ed
emtwnty <muizahsanu@gmail.com>
2020-04-03 01:19:50 +0700

    Memberikan fungsi button mulai pada MainActivity

--- app/src/main/java/com/example/akblatihan1/MainActivity.kt
+++ app/src/main/java/com/example/akblatihan1/MainActivity.kt
@@ -10,7 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        btn_mulai.setOnClickListener(){
+            val intent = Intent(this, DaftarActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
