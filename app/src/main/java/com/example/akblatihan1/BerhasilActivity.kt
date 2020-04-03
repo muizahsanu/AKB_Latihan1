0369553d15f45b47a8569a2db3854ef7b75a6a78
emtwnty <muizahsanu@gmail.com>
2020-04-03 14:08:23 +0700

    add new activity. mengatur dan membuat tampilan

---
+++ app/src/main/java/com/example/akblatihan1/BerhasilActivity.kt
@@ -0,0 +1,12 @@
+package com.example.akblatihan1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BerhasilActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_berhasil)
+    }
+}
