f3bacb8ba2fa24a7392326699aafbdbc00cb2fe6
emtwnty <muizahsanu@gmail.com>
2020-04-03 16:02:45 +0700

    validasi form. memberikan fungsi pada button. lempar nama antar activity

--- app/src/main/java/com/example/akblatihan1/BerhasilActivity.kt
+++ app/src/main/java/com/example/akblatihan1/BerhasilActivity.kt
@@ -2,11 +2,20 @@ package com.example.akblatihan1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import kotlinx.android.synthetic.main.activity_berhasil.*
 
 class BerhasilActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_berhasil)
+
+
+        val nama = intent.getStringExtra(EXTRA_MESSAGE)
+        tv_berhasil.setText(
+            "Beres! Sekarang "+nama +" udah bisa ngecek penggunaan HP mu tiap hari buat " +
+                    "bantu kamu ngatur waktu :)"
+        )
     }
 }
--- app/src/main/java/com/example/akblatihan1/BiodataActivity.kt
+++ app/src/main/java/com/example/akblatihan1/BiodataActivity.kt
@@ -1,12 +1,31 @@
 package com.example.akblatihan1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import kotlinx.android.synthetic.main.activity_biodata.*
 
 class BiodataActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_biodata)
+
+        btn_selanjutnya.setOnClickListener(){
+            val nama = frm_nama_biodata.text.toString()
+            val umur = frm_umur.text.toString()
+            if(nama.isEmpty()){
+                frm_nama_biodata.setError("Nama tidak boleh kosong")
+            }
+            else if(umur.isEmpty()){
+                frm_umur.setError("Umur tidak boleh kosong")
+            }
+            else{
+                val intent = Intent(this,BerhasilActivity::class.java)
+                    .putExtra(EXTRA_MESSAGE,nama)
+                startActivity(intent)
+            }
+        }
     }
 }
